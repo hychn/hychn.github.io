9769227793414cc3a320c3221756cbc92fdb0acd
hychn <daemon.ychnh@gmail.com>
2022-07-13 13:08:34 +0900

    update

--- tbl_contents.js
+++ tbl_contents.js
@@ -85,7 +85,7 @@ window.onload = function () {
     //var url = location.href;               //Saving URL without hash.
     //location.href = "#"+hash;                 //Navigate to the target element.
     //history.replaceState(null,null,url);   //method modifies the current history entry.
-    location.hash = '';
-    location.hash = hash;
+    window.location.hash = '';
+    window.location.hash = hash;
 
 };
