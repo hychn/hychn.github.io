8d18fc1af61d19eb7a2a9a9aea2cf3fbc6ac9cb5
hychn <daemon.ychnh@gmail.com>
2022-08-22 18:00:25 +0900

    update

--- md/subdef.py
+++ md/subdef.py
@@ -1,10 +1,13 @@
 o: H
 T: lTr
-^: LR
+^: LR 
+strips
 
 
 # landscaping idea
 * Things which look good from far, and pathable up close
 * sampler what sizing is reasonable for far away obj?
 * In other words... how can you reduce a voxel of different shapes/tex into a simpler mesh in a easy concise way
+* sample a 2x2, 4x4, 8x8, take the most frequent nonzero 
+* and make sure if drawing, a box that the nonzero blank elements are >=LxL/2 (2x1,4x2,8x4)
 
